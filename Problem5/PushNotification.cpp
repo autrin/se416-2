2b48b5ba679440aab76e81c70064b2bcc18bdb58
Autrin <autrinhakimi@gmail.com>
2025-03-27 12:55:27 -0500

    implement send method for PushNotification with console output

--- Problem5/PushNotification.cpp
+++ Problem5/PushNotification.cpp
@@ -1,3 +1,6 @@
 #include "PushNotification.hpp"
+#include "iostream"
 
-void PushNotification::send(std::string message) {}
+void PushNotification::send(std::string message) {
+  std::cout << "Hello by push" << std::endl;
+}
