02710a56c09608801b244ccea0a71a6c0339c774
Autrin <autrinhakimi@gmail.com>
2025-03-27 12:54:40 -0500

    implement SMSNotification send method with console output

--- Problem5/SMSNotification.cpp
+++ Problem5/SMSNotification.cpp
@@ -1,3 +1,6 @@
 #include "SMSNotification.hpp"
+#include "iostream"
 
-void SMSNotification::send(std::string message) {}
+void SMSNotification::send(std::string message) {
+  std::cout << "Hello by sms" << std::endl;
+}
